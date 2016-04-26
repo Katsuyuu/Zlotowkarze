315ea5c6038488adcd9467331b5e6d0146a6faa9
Adrian <goonygogo@gmail.com>
2016-04-27 00:31:58 +0200

    Wyświetlanie losowych pytań

--- Zlotowkarze/Controllers/HomeController.cs
+++ Zlotowkarze/Controllers/HomeController.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Zlotowkarze.Models.DAL;
 
 namespace Zlotowkarze.Controllers
 {
     public class HomeController : Controller
     {
+        QuestionContext db = new QuestionContext();
+
         public ActionResult LoginScreen()
         {
             return View();
@@ -15,7 +18,10 @@ namespace Zlotowkarze.Controllers
 
         public ActionResult Game()
         {
-            return View();
+            Random rand = new Random();
+            int randomQuestion = rand.Next(1, 101);
+
+            return View(db.Questions.Where(x => x.Id == randomQuestion).FirstOrDefault());
         }
     }
 }
